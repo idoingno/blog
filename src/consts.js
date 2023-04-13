251ab72eb053a846a19a029af71a7394511c9fee
Bai <idoingno@163.com>
2023-04-13 18:14:57 +0800

    update.

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `Idoingno's Blog`;
 export const SITE_DESCRIPTION = 'Idoingno Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_EMAIL = 'idoingno@gmail.com'
+export const SITE_NAME = 'vercel.uastro.eu.org';
+export const SITE_URL = "https://vercel.uastro.eu.org/";
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,9 +2,9 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function get() {
   return rss({
-    title: "Austin's Blog",
+    title: "Idoingno's Blog",
     description: "Site description",
-    site: 'https://astro-blog.qum.cc',
+    site: 'https://vercel.uastro.eu.org/',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>zh-cn</language>`,
   });
